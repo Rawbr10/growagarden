2a8d8eee3cdd3e0d5f946bf8122b6f29bf63cce0
Roblox Scripts <gregyheport@gmail.com>
2025-08-26 17:07:31 +0800

    Create delta2.lua

---
+++ delta2.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://github.com/Rawbr10/bloodball/raw/refs/heads/main/delta"))()
+task.wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Rawbr10/bloodball/refs/heads/main/loader2.lua"))()
